48f66c42bbb5dc35171d832c735c122ef46c5429
ATQQ <2604395430@qq.com>
2021-04-06 10:02:22 +0800

    feat: 引入ElementUI

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import http from "./utils/http";
 import api from "./api/index";
 import dateFormat from "./utils/dateFormat";
 import store from "./store";
+import './plugins/element.js'
 
 Vue.config.productionTip = false;
 Vue.prototype.$http = http;
